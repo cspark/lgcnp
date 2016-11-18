72d32d071085f8f5270d3b3d8edbb870cba9b364
Ray Jung <shakejj@gmail.com>
2016-11-18 12:00:46 +0900

    edited

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -4,7 +4,7 @@ set :deploy_to, "/home/janustabuser/lgcare"
 server 'janustabuser@203.247.132.106', roles: %w{web app db}
 
 set :branch, 'master'
-set :rails_env, 'production'
+set :rails_env, 'staging'
 set :unicorn_rack_env, "production"
 set :unicorn_pid, '/home/janustabuser/lgcare/shared/pids/unicorn.pid'
 set :unicorn_config_path, '/home/janustabuser/lgcare/current/config/unicorn/staging.rb'
