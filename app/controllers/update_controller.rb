53a191ba6d7ab8b61f17c74df9cf5d8e1c6f716c
Ray Jung <shakejj@gmail.com>
2016-11-29 12:07:05 +0900

    Update son

--- app/controllers/update_controller.rb
+++ app/controllers/update_controller.rb
@@ -5,7 +5,7 @@ class UpdateController < ApplicationController
     current_version_code = 12
 
     if version_code.to_i < current_version_code.to_i
-      url_json = "{'download_url' : 'http://203.247.132.106/app.apk'}"
+      url_json = { :download_url => 'http://203.247.132.106/app.apk' }
       render json: url_json.to_json, status: 200
     else
       render json: "", status: 200
