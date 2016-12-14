af393f44856b08c24d5d4e60f15c460539f58de9
Ray Jung <shakejj@gmail.com>
2016-12-14 17:56:50 +0900

    edit logger info

--- app/controllers/fcdatas_controller.rb
+++ app/controllers/fcdatas_controller.rb
@@ -54,7 +54,7 @@ class FcdatasController < ApplicationController
     ftp_path << user.custserial.to_i.to_s
     ftp_path << "-"
     ftp_path << face_data.measureno.to_s + "/2-1_F_FM_PL_1.jpg"
-    Rails.logger.logger ftp_path
+    Rails.logger.info ftp_path
     # system("wget http://www.hotel-r.net/im/hotel/de/d-d.gif -P public/CNP/")
     file_get_command = "wget --user janus --password pielgahn2012#1 "
     file_get_command << ftp_path
