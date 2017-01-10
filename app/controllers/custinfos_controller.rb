a08ecb690e9f377134ebac2cd66a7842214f466e
Ray Jung <shakejj@gmail.com>
2017-01-10 19:01:50 +0900

    Edited

--- app/controllers/custinfos_controller.rb
+++ app/controllers/custinfos_controller.rb
@@ -59,17 +59,31 @@ class CustinfosController < ApplicationController
   end
 
   def update_after_service
-      is_agree_after_service = params[:is_agree_after_service]
+      is_agree_after = params[:is_agree_after_service]
       serial = params[:serial]
 
       custinfo = Custinfo.where(custserial: serial).first
-      custinfo.is_agree_after_service = is_agree_after_service
+      custinfo.is_agree_after = is_agree_after
       if custinfo.save
         render json: custinfo.to_api_hash, status: 200
       else
         render json: "", status: 404
       end
   end
+
+
+    def update_email
+        email = params[:email]
+        serial = params[:serial]
+
+        custinfo = Custinfo.where(custserial: serial).first
+        custinfo.email = email
+        if custinfo.save
+          render json: custinfo.to_api_hash, status: 200
+        else
+          render json: "", status: 404
+        end
+    end
 
 
   def update_agreement
