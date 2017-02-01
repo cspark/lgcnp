f543ea6c7e20f85d10fe0d158488918899b28972
Ray Jung <shakejj@gmail.com>
2017-02-01 21:48:22 +0900

    Edited

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,6 +1,7 @@
 class AdminUser < ApplicationRecord
+  self.table_name = "adminuser" if Rails.env.production?
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, 
+  devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable
 end
