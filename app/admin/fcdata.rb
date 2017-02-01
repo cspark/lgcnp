79d634942c515969f43915916bbe5b621bf6e535
Ray Jung <shakejj@gmail.com>
2017-02-01 22:41:38 +0900

    Edited

--- app/admin/fcdata.rb
+++ app/admin/fcdata.rb
@@ -2,7 +2,6 @@ ActiveAdmin.register Fcdata do
   config.clear_action_items!
   index do
     selectable_column
-    id_column
     Fctabletinterview.column_names.each do |c|
       column c.to_sym
     end
