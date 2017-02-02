ca82b9cc5a2084db8edf62b43e7f56350e8089d9
Ray Jung <shakejj@gmail.com>
2017-02-02 18:40:24 +0900

    Edited

--- app/controllers/feedback_controller.rb
+++ app/controllers/feedback_controller.rb
@@ -8,9 +8,9 @@ class FeedbackController < ApplicationController
     @date = params[:date] ? Date.parse(params[:date]) : Date.today
 
     if Rails.env.production? || Rails.env.staging?
-      @tablet_interviews_1_days_ago = Fctabletinterview.where("to_date(uptdate) >= ? AND to_date(uptdate) <= ?", (Date.today - 2.days), (Date.today))
-      @tablet_interviews_2_weeks_ago = Fctabletinterview.where("to_date(uptdate) >= ? AND to_date(uptdate) <= ?", (Date.today - 2.weeks - 1.days), (Date.today - 2.weeks + 1.days))
-      @tablet_interviews_3_months_ago = Fctabletinterview.where("to_date(uptdate) >= ? AND to_date(uptdate) <= ?", (Date.today - 3.months - 1.days), (Date.today - 3.months + 1.days))
+      @tablet_interviews_1_days_ago = Fctabletinterview.where("to_date(uptdate) >= ? AND to_date(uptdate) <= ?", (@date - 2.days), (@date))
+      @tablet_interviews_2_weeks_ago = Fctabletinterview.where("to_date(uptdate) >= ? AND to_date(uptdate) <= ?", (@date - 2.weeks - 1.days), (@date - 2.weeks + 1.days))
+      @tablet_interviews_3_months_ago = Fctabletinterview.where("to_date(uptdate) >= ? AND to_date(uptdate) <= ?", (@date - 3.months - 1.days), (@date - 3.months + 1.days))
     else
       @tablet_interviews_1_days_ago = Fctabletinterview.all
       @tablet_interviews_2_weeks_ago = Fctabletinterview.all
