63006183cb674302dd478f66c3c60099e684e8bf
kimsoomin <soomin0322@naver.com>
2017-02-22 22:18:00 +0900

    edit

--- app/controllers/admin/schedule_controller.rb
+++ app/controllers/admin/schedule_controller.rb
@@ -3,7 +3,21 @@ class Admin::ScheduleController < Admin::AdminApplicationController
   before_action :is_admin
 
   def list
-    @fcschedules = Fcschedule.all.order("uptdate desc")
+    # Time.current.year
+    if !params[:select_date].blank?
+      @select_date = params[:select_date]
+    else
+      @select_date = Date.today
+    end
+
+    select_year = @select_date.to_s.split("-")[0]
+    select_mmdd = @select_date.to_s.split("-")[1]+@select_date.to_s.split("-")[2]
+
+    scoped = Fcschedule.all
+    scoped = scoped.where("reserve_yyyy = ?", select_year)
+    scoped = scoped.where("reserve_mmdd = ?", select_mmdd)
+    @fcschedules = scoped.order("uptdate desc")
+
     @fcschedules = Kaminari.paginate_array(@fcschedules).page(params[:page]).per(3)
   end
 end
