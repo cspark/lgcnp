4ff869823c678e4066d4679d0891aba585af67fc
kimsoomin <soomin0322@naver.com>
2017-02-27 18:51:46 +0900

    edit

--- app/controllers/api/beau/beau_fcavgdata_controller.rb
+++ app/controllers/api/beau/beau_fcavgdata_controller.rb
@@ -1,6 +1,6 @@
 class Api::Beau::BeauFcavgdataController < Api::ApplicationController
   def index
-    list = Fcavgdata.all.order("n_index asc")
+    list = Fcavgdata.all.order("n_index asc").page(params[:page]).per(params[:per])
     if list.count > 0
       render json: api_hash_for_list(list), status: :ok
     else
