eab3d259661a6a2d209e1a0ea1348b4cfecde5be
kimsoomin <soomin0322@naver.com>
2017-02-28 18:45:23 +0900

    edit

--- app/models/lcare_user.rb
+++ app/models/lcare_user.rb
@@ -3,10 +3,8 @@ class LcareUser < ApplicationRecord
   establish_connection(
     :adapter  => 'oracle_enhanced',
     :database => '(DESCRIPTION=(FAILOVER=on)(LOAD_BALANCE=off)(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=165.244.88.137)(PORT=1528))(ADDRESS=(PROTOCOL=TCP)(HOST=165.244.88.138)(PORT=1528)))(CONNECT_DATA=(FAILOVER_MODE=(TYPE=select)(METHOD=basic)(RETRIES=20)(DELAY=3))(SERVER=dedicated)(SERVICE_NAME=LGCIO)))',
-    :host     => '165.244.88.137',
     :username => 'ifuser',
-    :password => 'ifuser!123',
-    :port     => 1528
+    :password => 'ifuser!123'
   )
   self.table_name = "if_tdc10" if Rails.env.production? || Rails.env.staging?
 
@@ -24,8 +22,9 @@ class LcareUser < ApplicationRecord
 
   def self.list(cust_hnm: nil, birth_year: nil, birth_mmdd: nil, cell_phnno: nil, page: 1, per: 3)
     Rails.logger.info "self.list!!!"
+    Rails.logger.info count
     scoped = LcareUser.all
-    Rails.logger.info scoped
+    Rails.logger.info scoped.count
     scoped = scoped.where(cust_hnm: cust_hnm) if cust_hnm.present?
     scoped = scoped.where(birthyy: birthyy) if birthyy.present?
     scoped = scoped.where(birth_mmdd: birth_mmdd) if birth_mmdd.present?
