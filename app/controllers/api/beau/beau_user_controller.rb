7fc87ca2fe2329542aa4eb4793110f3b9c6e8e0e
kimsoomin <soomin0322@naver.com>
2017-03-02 18:34:11 +0900

    edit

--- app/controllers/api/beau/beau_user_controller.rb
+++ app/controllers/api/beau/beau_user_controller.rb
@@ -86,6 +86,6 @@ class Api::Beau::BeauUserController < Api::ApplicationController
 
   private
   def permitted_params
-    params.permit(:custserial, :ch_cd, :n_cust_id, :custname, :sex, :age, :birthyy, :birthmm, :birthdd, :phone, :uptdate, :shop_cd, :is_agree_privacy, :is_agree_after, :is_agree_marketing, :is_agree_thirdparty_info, :address)
+    params.permit(:custserial, :ch_cd, :n_cust_id, :custname, :sex, :age, :birthyy, :birthmm, :birthdd, :phone, :uptdate, :shop_cd, :is_agree_privacy, :is_agree_after, :is_agree_marketing, :is_agree_thirdparty_info, :address, :lastanaldate)
   end
 end
