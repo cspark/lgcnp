4973e23c71ba87b8c7b99fcd8adba9ae5db18e63
kimsoomin <soomin0322@naver.com>
2017-03-08 11:01:43 +0900

    edit

--- config/schedule.rb
+++ config/schedule.rb
@@ -5,6 +5,6 @@ ActiveSupport::TimeZone.all.each do |timezone|
   Time.zone = timezone.name
 
   every :day, :at => Time.zone.parse('11:10 am').utc do
-    rake "image:remove_image[#{timezone.name}]"
+    rake "image:remove_image"
   end
 end
