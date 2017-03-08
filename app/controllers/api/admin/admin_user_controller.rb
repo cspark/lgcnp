a12efe10d8f22d9a3d8a65dd786ed5b8221e536b
kimsoomin <soomin0322@naver.com>
2017-03-08 16:17:10 +0900

    edit

--- app/controllers/api/admin/admin_user_controller.rb
+++ app/controllers/api/admin/admin_user_controller.rb
@@ -10,65 +10,6 @@ class Api::Admin::AdminUserController < Api::ApplicationController
   end
 
   def update
-    # L-Care 통합회원 조회 후 L-Care 고객정보의 핸드폰번호를  Janus3 DB 고객정보 핸드폰번호 Update
-    user = Custinfo.where(custserial: params[:id]).first
-    if !user.nil?
-      if params[:phone].present?
-        user.phone = params[:phone]
-      end
-      if user.save
-        render json: user.to_api_hash, status: :ok
-      else
-        render json: "", status: 404
-      end
-    else
-      render json: "", status: 404
-    end
-  end
-
-  def lcare_user_list
-    # L-Care Serial 조건으로 Janus3 DB에 해당 L-Care 회원이 존재하는지 확인
-    user = Custinfo.where(n_cust_id: params[:n_cust_id]).first
-    if !user.nil?
-      render json: user.to_api_hash, status: :ok
-    else
-      render json: "", status: 404
-    end
-  end
-
-  def show
-    user = Custinfo.where(custserial: params[:id]).first
-    if !user.nil?
-      render json: user.to_api_hash, status: :ok
-    else
-      render json: "", status: 404
-    end
-  end
-
-  def create
-    # 고객정보를 insert 하기 위하여 Max(CUSTSERIAL) 값을 구하여 +1
-    # L-care 조회하여 받은 회원 정보로 janus3 회원정보 insert
-
-    user = Custinfo.new(permitted_params)
-    if Custinfo.all.count > 0
-      custserial = Custinfo.all.order('CAST(custserial AS INT) desc').first.custserial.to_i + 1
-    else
-      custserial = 1.to_s
-    end
-      user.custserial = custserial.to_s
-
-    t = Time.now
-    yymmdd = t.to_s.split(" ")[0]
-    user.uptdate = yymmdd.split("-")[0] +"/"+ yymmdd.split("-")[1] +"/"+ yymmdd.split("-")[2]
-
-    if user.save
-      render json: user.to_api_hash, status: :ok
-    else
-      render json: "", status: 404
-    end
-  end
-
-  def update
     # Janus3 고객DB에 존재하는 고객인 경우 L-Care 핸드폰번호만 Update
     # address 추가
     user = Custinfo.where(custserial: params[:id]).first
@@ -96,17 +37,8 @@ class Api::Admin::AdminUserController < Api::ApplicationController
     end
   end
 
-  def measure_update
-    # Data 분석이 완료 된 후 CUSTINFO의 분석횟수 카운트 증가시키고, 최근 분석일 Update
-    user = Custinfo.where(custserial: params[:custserial]).first
-    if !user.nil?
-      user.increase_measureno
-      user.update_lastanaldate
-      user.save
-      render json: user.to_api_hash, status: :ok
-    else
-      render json: "", status: 404
-    end
+  def destroy
+    list = Custinfo.where(custserial: params[:id])
   end
 
   private
