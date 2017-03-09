a30f0584bb7e8a17f9b06d5bd9c685e94a37b9b3
kimsoomin <soomin0322@naver.com>
2017-03-09 19:41:56 +0900

    edit

--- app/admin/fctabletinterview.rb
+++ app/admin/fctabletinterview.rb
@@ -1,1 +1,20 @@
-
+# ActiveAdmin.register Fctabletinterview do
+#   config.clear_action_items!
+#   index do
+#     selectable_column
+#     Fctabletinterview.column_names.each do |c|
+#       column c.to_sym
+#     end
+#
+#     # column :custname
+#     # column :sex
+#     # column :birthyy
+#     # column :birthmm
+#     # column :birthdd
+#     # column :phone
+#     # column :address
+#     # column :email
+#     # column :uptdate
+#     # actions
+#   end
+# end
--- config/schedule.rb
+++ config/schedule.rb
@@ -1,8 +1,12 @@
 require 'active_support'
 require 'active_support/time'
 
-every :day, :at => '7:25 pm' do
-  command "mkdir public/TEST"
-  system("mkdir public/TEST2")
-  rake "image:remove_image"
+every :day, :at => '4:00 am' do
+  system("rm -rf public/BEAU")
+  system("rm -rf public/CNP")
+  system("rm -rf public/CLAB")
+  system("rm -rf public/LABO")
+  system("rm -rf public/MART")
+  system("rm -rf public/TMR")
+  # rake "image:remove_image"
 end
