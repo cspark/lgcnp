b0c453237891edd023f277965bec0cf71b3137e8
kimsoomin <soomin0322@naver.com>
2017-03-09 19:56:35 +0900

    edit

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,10 +2,5 @@ require 'active_support'
 require 'active_support/time'
 
 every :day, :at => '4:00 am' do
-  system("rm -rf public/BEAU")
-  system("rm -rf public/CNP")
-  system("rm -rf public/CLAB")
-  system("rm -rf public/LABO")
-  system("rm -rf public/MART")
-  system("rm -rf public/TMR")
+  rake "image:remove_image"
 end
