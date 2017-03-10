331e43c6a9cf21faaa59a1e89b6bf8eb78f04f5c
kimsoomin <soomin0322@naver.com>
2017-03-10 10:10:16 +0900

    edit

--- app/admin/fcdata.rb
+++ app/admin/fcdata.rb
@@ -1,9 +1,1 @@
-# ActiveAdmin.register Fcdata do
-#   config.clear_action_items!
-#   index do
-#     selectable_column
-#     Fctabletinterview.column_names.each do |c|
-#       column c.to_sym
-#     end
-#   end
-# end
+
