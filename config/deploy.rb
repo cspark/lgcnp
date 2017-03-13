80c140d0d80e15f7f10ac8093bec3f741d705c7e
kimsoomin <soomin0322@naver.com>
2017-03-13 17:07:13 +0900

    edit

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,7 +26,6 @@ set :port, 10022
 # set :linked_files, %w{config/database.yml config/secrets.yml}
 # set :linked_dirs, fetch(:linked_dirs, []).push('public/system', 'log', 'public/uploads')
 
-set :whenever_roles,        ->{ :app }
 set :whenever_identifier, ->{ "#{fetch(:application)}_#{fetch(:stage)}" }
 
 # Default branch is :master
@@ -58,11 +57,6 @@ set :whenever_identifier, ->{ "#{fetch(:application)}_#{fetch(:stage)}" }
 
 # Default value for keep_releases is 5
 # set :keep_releases, 5
-namespace :whenever do
-  task :start, :roles => :app do
-    run "cd #{release_path} && bundle exec whenever --update-crontab"
-  end
-end
 
 namespace :deploy do
   desc 'Restart application'
