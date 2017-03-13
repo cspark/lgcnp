591359f3c2d5f2317893b2ea36123b782d77cfc1
kimsoomin <soomin0322@naver.com>
2017-03-13 21:20:53 +0900

    edit

--- app/models/login_history.rb
+++ app/models/login_history.rb
@@ -1,5 +1,7 @@
 class LoginHistory < ApplicationRecord
   self.table_name = "loginhistory" if Rails.env.production? || Rails.env.staging?
-  set_datetime_columns :created_at, :updated_at
+  # set_datetime_columns :created_at, :updated_at
+  attribute :created_at, :datetime
+  attribute :updated_at, :datetime
 
 end
