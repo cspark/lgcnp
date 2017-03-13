f049a0db4af1930e97d28f03246b4b4537c5a03e
kimsoomin <soomin0322@naver.com>
2017-03-13 18:11:28 +0900

    edit

--- config/deploy.rb
+++ config/deploy.rb
@@ -56,9 +56,41 @@ set :port, 10022
 
 # Default value for keep_releases is 5
 # set :keep_releases, 5
-namespace :deploy do
-  before :restart, :update_crontab do
-    # run "cd #{release_path} && bundle exec whenever --update-crontab"
+
+namespace :whenever do
+  def setup_whenever_task(*args, &block)
+    args = Array(fetch(:whenever_command)) + args
+
+    on roles fetch(:whenever_roles) do |host|
+      args_for_host = block_given? ? args + Array(yield(host)) : args
+      within release_path do
+        with fetch(:whenever_command_environment_variables) do
+          execute(*args_for_host)
+        end
+      end
+    end
+  end
+
+  desc "Update application's crontab entries using Whenever"
+  task :update_crontab do
+    setup_whenever_task do |host|
+      roles = host.roles_array.join(",")
+      [fetch(:whenever_update_flags),  "--roles=#{roles}"]
+    end
+  end
+
+  desc "Clear application's crontab entries using Whenever"
+  task :clear_crontab do
+    setup_whenever_task(fetch(:whenever_clear_flags))
+  end
+
+  after "deploy:updated",  "whenever:update_crontab"
+  after "deploy:reverted", "whenever:update_crontab"
+end
+
+namespace :load do
+  task :defaults do
+    set :whenever_roles,        ->{ :db }
     set :whenever_command,      ->{ [:bundle, :exec, :whenever] }
     set :whenever_command_environment_variables, ->{ { rails_env: fetch(:whenever_environment) } }
     set :whenever_identifier,   ->{ fetch :application }
@@ -66,6 +98,12 @@ namespace :deploy do
     set :whenever_variables,    ->{ "environment=#{fetch :whenever_environment}" }
     set :whenever_update_flags, ->{ "--update-crontab #{fetch :whenever_identifier} --set #{fetch :whenever_variables}" }
     set :whenever_clear_flags,  ->{ "--clear-crontab #{fetch :whenever_identifier}" }
+  end
+end
+
+namespace :deploy do
+  before :restart, :update_crontab do
+    # run "cd #{release_path} && bundle exec whenever --update-crontab"
   end
 
   desc 'Restart application'
