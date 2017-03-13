cc6df9c770fffafde3979820673745c115792515
kimsoomin <soomin0322@naver.com>
2017-03-13 17:58:10 +0900

    edit

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,7 +26,7 @@ set :port, 10022
 # set :linked_dirs, fetch(:linked_dirs, []).push('public/system', 'log', 'public/uploads')
 
 
-set :whenever_command, "bundle exec whenever"
+set :whenever_identifier, ->{ "#{fetch(:application)}_#{fetch(:stage)}" }
 require 'whenever/capistrano'
 
 # Default branch is :master
