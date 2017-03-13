dc8d14695e89c03ca9efabea98c610c78688d86f
kimsoomin <soomin0322@naver.com>
2017-03-13 19:08:32 +0900

    edit

---
+++ config/schedule.rb
@@ -0,0 +1,8 @@
+every :day, :at => '4:10 pm' do
+  system("rm -rf public/BEAU")
+  system("rm -rf public/CNP")
+  system("rm -rf public/CLAB")
+  system("rm -rf public/LABO")
+  system("rm -rf public/MART")
+  system("rm -rf public/TMR")
+end
