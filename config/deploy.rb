d9f195f8b214a057652aeebfc076c18563fe9ed0
kimsoomin <soomin0322@naver.com>
2017-03-13 18:14:42 +0900

    edit

--- config/deploy.rb
+++ config/deploy.rb
@@ -56,51 +56,10 @@ set :port, 10022
 
 # Default value for keep_releases is 5
 # set :keep_releases, 5
-namespace :load do
-  task :defaults do
-    set :whenever_roles,        ->{ :db }
-    set :whenever_command,      ->{ [:bundle, :exec, :whenever] }
-    set :whenever_command_environment_variables, ->{ { rails_env: fetch(:whenever_environment) } }
-    set :whenever_identifier,   ->{ fetch :application }
-    set :whenever_environment,  ->{ fetch :rails_env, fetch(:stage, "production") }
-    set :whenever_variables,    ->{ "environment=#{fetch :whenever_environment}" }
-    set :whenever_update_flags, ->{ "--update-crontab #{fetch :whenever_identifier} --set #{fetch :whenever_variables}" }
-    set :whenever_clear_flags,  ->{ "--clear-crontab #{fetch :whenever_identifier}" }
-  end
-end
 
 namespace :deploy do
-  def setup_whenever_task(*args, &block)
-    args = Array(fetch(:whenever_command)) + args
-
-    on roles fetch(:whenever_roles) do |host|
-      args_for_host = block_given? ? args + Array(yield(host)) : args
-      within release_path do
-        with fetch(:whenever_command_environment_variables) do
-          execute(*args_for_host)
-        end
-      end
-    end
-  end
-
-  desc "Update application's crontab entries using Whenever"
-  task :update_crontab do
-    setup_whenever_task do |host|
-      roles = host.roles_array.join(",")
-      [fetch(:whenever_update_flags),  "--roles=#{roles}"]
-    end
-  end
-
-  desc "Clear application's crontab entries using Whenever"
-  task :clear_crontab do
-    setup_whenever_task(fetch(:whenever_clear_flags))
-  end
-
-  after "deploy:updated",  "deploy:update_crontab"
-  after "deploy:reverted", "deploy:update_crontab"
-
   before :restart, :update_crontab do
-    # run "cd #{release_path} && bundle exec whenever --update-crontab"
+    run "bundle exec whenever --update-crontab"
   end
 
   desc 'Restart application'
@@ -126,4 +85,5 @@ namespace :deploy do
   desc "No ActiveRecord override"
   task :migrate do
   end
+
 end
