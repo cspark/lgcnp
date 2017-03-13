3db99807ca72fc942bc8368892c047d1d8edc84a
kimsoomin <soomin0322@naver.com>
2017-03-13 16:14:18 +0900

    edit

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,7 +25,6 @@ set :port, 10022
 # set :linked_files, %w{config/database.yml config/secrets.yml}
 # set :linked_dirs, fetch(:linked_dirs, []).push('public/system', 'log', 'public/uploads')
 
-set :whenever_environment, defer { stage }
 set :whenever_identifier, defer { "#{application}-#{stage}" }
 
 require 'whenever/capistrano'
