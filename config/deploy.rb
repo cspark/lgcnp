459737fdf124f454bf9b7f42fdf33f75852e7f0c
kimsoomin <soomin0322@naver.com>
2017-03-13 17:12:49 +0900

    edit

--- config/deploy.rb
+++ config/deploy.rb
@@ -58,13 +58,14 @@ set :whenever_identifier, ->{ "#{fetch(:application)}_#{fetch(:stage)}" }
 
 # Default value for keep_releases is 5
 # set :keep_releases, 5
+namespace :whenever do
+  task :start, :roles => :app do
+    run "cd #{release_path} && bundle exec whenever --update-crontab"
+  end
+  after "deploy:update", "whenever:start"
+end
 
 namespace :deploy do
-  desc "Update the crontab file"
-  task :update_crontab, :roles => :app, :except => { :no_release => true } do
-    run "cd #{release_path} && bundle exec whenever --update-crontab #{application}"
-  end
-
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
@@ -89,5 +90,4 @@ namespace :deploy do
   task :migrate do
   end
 
-  after 'deploy:update_code', 'deploy:update_crontab'
 end
