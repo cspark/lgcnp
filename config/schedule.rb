661c585ab2654f1d4b0c06ecbe97ea9cdade8570
kimsoomin <soomin0322@naver.com>
2017-03-13 15:47:11 +0900

    edit

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,6 +2,5 @@ require 'active_support'
 require 'active_support/time'
 
 every :day, :at => '10:30 am' do
-  run "cd #{release_path} && bundle exec whenever --update-crontab #{application}"
   rake "image:remove_image"
 end
