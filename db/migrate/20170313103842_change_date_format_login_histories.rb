e7ce5942189976863e5b16950db6e075f5b04c5d
kimsoomin <soomin0322@naver.com>
2017-03-13 19:52:59 +0900

    edit

--- db/migrate/20170313103842_change_date_format_login_histories.rb
+++ db/migrate/20170313103842_change_date_format_login_histories.rb
@@ -1,11 +1,4 @@
 class ChangeDateFormatLoginHistories < ActiveRecord::Migration[5.0]
-  def up
-    change_column :login_histories, :created_at, :datetime
-    change_column :login_histories, :updated_at, :datetime
-  end
-
-  def down
-    change_column :login_histories, :created_at, :date
-    change_column :login_histories, :updated_at, :date
-  end
+  change_column :login_histories, :created_at, :datetime
+  change_column :login_histories, :updated_at, :datetime
 end
