a7c43169c239654202ce079aa785c7ea26828ad3
kimsoomin <soomin0322@naver.com>
2017-03-16 14:32:34 +0900

    edit

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,8 +1,17 @@
-every :day, :at => '6:40 pm' do
+every :day, :at => '4:00 am' do
   system("rm -rf public/BEAU")
   system("rm -rf public/CNP")
   system("rm -rf public/CLAB")
   system("rm -rf public/LABO")
   system("rm -rf public/MART")
   system("rm -rf public/TMR")
+end
+
+every :day, :at => '4:01 am' do
+  system("mkdir public/BEAU")
+  system("mkdir public/CNP")
+  system("mkdir public/CLAB")
+  system("mkdir public/LABO")
+  system("mkdir public/MART")
+  system("mkdir public/TMR")
 end
