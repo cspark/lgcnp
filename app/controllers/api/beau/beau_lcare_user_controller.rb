8fd3092514c99f2454a072ffe1752fd86db8c098
kimsoomin <soomin0322@naver.com>
2017-03-23 16:39:17 +0900

    edit

--- app/controllers/api/beau/beau_lcare_user_controller.rb
+++ app/controllers/api/beau/beau_lcare_user_controller.rb
@@ -2,17 +2,16 @@ class Api::Beau::BeauLcareUserController < Api::ApplicationController
   def lcare_integrated_user_list
     # L-Care 통합회원 조회 이름, 생년월일, 핸드폰번호, 통합회원여부 조건으로 고객정보 조회 (* Next 조회 필요)
     if params.has_key?(:cell_phnno)
-      lcare_user = LcareUser.list(cust_hnm: params[:cust_hnm], birth_year: params[:birth_year], birth_mmdd: params[:birth_mmdd], cell_phnno: params[:cell_phnno])
+      lcare_user = LcareUser.where(cust_hnm: params[:cust_hnm], birth_year: params[:birth_year], birth_mmdd: params[:birth_mmdd], cell_phnno: params[:cell_phnno]).first
     else
-      lcare_user = LcareUser.list(cust_hnm: params[:cust_hnm], birth_year: params[:birth_year], birth_mmdd: params[:birth_mmdd])
+      lcare_user = LcareUser.where(cust_hnm: params[:cust_hnm], birth_year: params[:birth_year], birth_mmdd: params[:birth_mmdd]).first
     end
 
-    if lcare_user.count > 0
-      Rails.logger.info lcare_user.count
-      Rails.logger.info lcare_user.first
-      Rails.logger.info ActiveSupport::JSON.encode(api_hash_for_list(lcare_user)).size+4
-      response.set_header("Content-length", ActiveSupport::JSON.encode(api_hash_for_list(lcare_user)).size+4)
-      render json: api_hash_for_list(lcare_user), status: 200
+    if !lcare_user.nil?
+      Rails.logger.info lcare_user
+      Rails.logger.info ActiveSupport::JSON.encode(lcare_user.to_api_hash).size
+      response.set_header("Content-length", ActiveSupport::JSON.encode(lcare_user.to_api_hash).size)
+      render json: lcare_user.to_api_hash, status: :ok
     else
       render :text => "Lcare user is not exist!!!", status: 204
     end
