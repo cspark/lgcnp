8455a9116e8ef3bd3446c8b6f3198467bda08522
kimsoomin <soomin0322@naver.com>
2017-03-23 15:31:29 +0900

    edit

--- app/controllers/api/beau/beau_lcare_user_controller.rb
+++ app/controllers/api/beau/beau_lcare_user_controller.rb
@@ -9,8 +9,9 @@ class Api::Beau::BeauLcareUserController < Api::ApplicationController
 
     if lcare_user.count > 0
       Rails.logger.info lcare_user.count
+      Rails.logger.info lcare_user
       Rails.logger.info ActiveSupport::JSON.encode(api_hash_for_list(lcare_user)).size
-      response.set_header("content-length", ActiveSupport::JSON.encode(api_hash_for_list(lcare_user)).size)
+      response.set_header("Content-length", ActiveSupport::JSON.encode(api_hash_for_list(lcare_user)).size)
       render json: api_hash_for_list(lcare_user), status: :ok
     else
       render :text => "Lcare user is not exist!!!", status: 204
