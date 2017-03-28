47b7c39d74ba914f9f8db19ffbb0aa66371305c9
kimsoomin <soomin0322@naver.com>
2017-03-28 11:56:32 +0900

    edit

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,19 +1,26 @@
-every :day, :at => Time.zone.parse('11:35 am').utc do
-  system("rm -rf public/BEAU")
-  system("rm -rf public/CNP")
-  system("rm -rf public/CLAB")
-  system("rm -rf public/LABO")
-  system("rm -rf public/MART")
-  system("rm -rf public/TMR")
-  notifier = Slack::Notifier.new "https://hooks.slack.com/services/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"
-  notifier.ping "CNP Image Remove Execute !!!"
-end
+require 'active_support'
+require 'active_support/time'
 
-# every :day, :at => Time.zone.parse('11:36 am').utc do
-#   system("mkdir public/BEAU")
-#   system("mkdir public/CNP")
-#   system("mkdir public/CLAB")
-#   system("mkdir public/LABO")
-#   system("mkdir public/MART")
-#   system("mkdir public/TMR")
-# end
+ActiveSupport::TimeZone.all.each do |timezone|
+  Time.zone = timezone.name
+
+  every :day, :at => Time.zone.parse('00:10 pm').utc do
+    system("rm -rf public/BEAU")
+    system("rm -rf public/CNP")
+    system("rm -rf public/CLAB")
+    system("rm -rf public/LABO")
+    system("rm -rf public/MART")
+    system("rm -rf public/TMR")
+  end
+
+  every :day, :at => Time.zone.parse('00:11 pm').utc do
+    system("mkdir public/BEAU")
+    system("mkdir public/CNP")
+    system("mkdir public/CLAB")
+    system("mkdir public/LABO")
+    system("mkdir public/MART")
+    system("mkdir public/TMR")
+    notifier = Slack::Notifier.new "https://hooks.slack.com/services/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"
+    notifier.ping "CNP Image Remove Execute !!!"
+  end
+end
