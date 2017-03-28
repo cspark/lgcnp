2ebc7b9f09d4c367fc6e306dd4c0c6d14bc7e4af
kimsoomin <soomin0322@naver.com>
2017-03-28 17:51:20 +0900

    edit

--- config/schedule.rb
+++ config/schedule.rb
@@ -4,11 +4,8 @@ require 'active_support/time'
 ActiveSupport::TimeZone.all.each do |timezone|
   Time.zone = timezone.name
   # Rails.logger.info Time.zone
-  # Rails.logger.info Time.zone.parse('02:30 am').utc
-  every :day, :at => Time.zone.parse('02:30 am').utc do
-    rake "image:remove_image"
-  end
-  every :day, :at => Time.zone.parse('02:40 am').utc do
+  # Rails.logger.info Time.zone.parse('03:00 am').utc
+  every :day, :at => Time.zone.parse('03:00 am').utc do
     rake "image:remove_image"
   end
   # every :day, :at => Time.zone.parse('01:00 pm').utc do
