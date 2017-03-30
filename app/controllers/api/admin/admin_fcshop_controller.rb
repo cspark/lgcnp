558e2e4efb61ec230e25bfb4beb8f65e62777f3a
kimsoomin <soomin0322@naver.com>
2017-03-30 14:32:23 +0900

    edit

--- app/controllers/api/admin/admin_fcshop_controller.rb
+++ app/controllers/api/admin/admin_fcshop_controller.rb
@@ -1,8 +1,14 @@
 class Api::Admin::AdminFcshopController < Api::ApplicationController
   def index
     # 매장 테이블 전체 조회 (* Next 조회 필요)
-    list = Fcshop.all.order("shop_cd asc")
-    render json: api_hash_for_list(list), status: :ok
+    list = Fcshop.list(shop_cd: params[:shop_cd])
+    if list.nil?
+      render :text => "Fail!!!", status: 404
+    elsif list.count > 0
+      render json: api_hash_for_list(list), status: :ok
+    else
+      render :text => "Shop not exist!!!", status: 204
+    end
   end
 
   def create
