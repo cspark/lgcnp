e8802cddf728a923114ed5f5a99fee2c6894d1e6
kimsoomin <soomin0322@naver.com>
2017-03-31 15:37:06 +0900

    edit

--- config/schedule.rb
+++ config/schedule.rb
@@ -9,6 +9,10 @@
 # end
 # 01:00 pm -> 새벽 4시
 
-every :day, :at => '00:10 am' do
+# every :day, :at => '00:40am' do
+#   rake "image:remove_image"
+# end
+
+every :day, :at => '03:40pm' do
   rake "image:remove_image"
 end
