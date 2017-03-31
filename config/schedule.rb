8d77afa49c6a236e50ebb9d75923e4056872624e
kimsoomin <soomin0322@naver.com>
2017-03-31 16:00:16 +0900

    edit

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,18 +1,5 @@
-# require 'active_support'
-# require 'active_support/time'
-#
-# ActiveSupport::TimeZone.all.each do |timezone|
-#   Time.zone = timezone.name
-#   every :day, :at => Time.zone.parse('01:00 pm').utc do
-#     rake "image:remove_image"
-#   end
-# end
 # 01:00 pm -> 새벽 4시
 
-# every :day, :at => '00:40am' do
-#   rake "image:remove_image"
-# end
-
-every :day, :at => '03:40pm' do
+every :day, :at => '04:30pm' do
   rake "image:remove_image"
 end
