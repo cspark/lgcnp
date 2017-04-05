d40f7191d8f50671add7c7b414b345e2728014f4
kimsoomin <soomin0322@naver.com>
2017-04-05 10:28:54 +0900

    edit

--- app/controllers/admin/pos_controller.rb
+++ app/controllers/admin/pos_controller.rb
@@ -39,9 +39,107 @@ class Admin::PosController < Admin::AdminApplicationController
       "우측눈 사각영역 정보l","우측눈 사각영역 정보t","우측눈 사각영역 정보r","우측눈 사각영역 정보b","좌측눈 사각영역 정보l","좌측눈 사각영역 정보t","좌측눈 사각영역 정보r","좌측눈 사각영역 정보b","입 사각영역 정보l","입 사각영역 정보t","입 사각영역 정보r","입 사각영역 정보b",
       "우측 얼굴 끝 라인 좌표x","우측 얼굴 끝 라인 좌표y","좌측 얼굴 끝 라인 좌표x","좌측 얼굴 끝 라인 좌표y"]
     else
-      # @fcpos = @fcpos.select("custserial,ch_cd,faceno,measuredate,measureno,uptdate," +@params_filter)
       @excel_name = ["이름","채널구분","전면/좌/우측면","분석 일","분석 횟수","업데이트 일"]
       @select_filter.each do |filter|
+        if filter.include?("fh_x")
+          filter = "이마 영역정보x"
+        elsif filter.include?("fh_y")
+          filter = "이마 영역정보y"
+        elsif filter.include?("fh_w")
+          filter = "이마 영역정보w"
+        elsif filter.include?("fh_h")
+          filter = "이마 영역정보h"
+        elsif filter.include?("ns_x")
+          filter = "코 영역정보x"
+        elsif filter.include?("ns_y")
+          filter = "코 영역정보y"
+        elsif filter.include?("ns_w")
+          filter = "코 영역정보w"
+        elsif filter.include?("ns_h")
+          filter = "코 영역정보h"
+        elsif filter.include?("res_x")
+          filter = "우측 눈가 영역정보x"
+        elsif filter.include?("res_y")
+          filter = "우측 눈가 영역정보y"
+        elsif filter.include?("res_w")
+          filter = "우측 눈가 영역정보w"
+        elsif filter.include?("res_h")
+          filter = "우측 눈가 영역정보h"
+        elsif filter.include?("reu_x")
+          filter = "우측 눈밑 영역정보x"
+        elsif filter.include?("reu_y")
+          filter = "우측 눈밑 영역정보y"
+        elsif filter.include?("reu_w")
+          filter = "우측 눈밑 영역정보w"
+        elsif filter.include?("reu_h")
+          filter = "우측 눈밑 영역정보h"
+        elsif filter.include?("les_x")
+          filter = "좌측 눈가 영역정보x"
+        elsif filter.include?("les_y")
+          filter = "좌측 눈가 영역정보y"
+        elsif filter.include?("les_w")
+          filter = "좌측 눈가 영역정보w"
+        elsif filter.include?("les_h")
+          filter = "좌측 눈가 영역정보h"
+        elsif filter.include?("leu_x")
+          filter = "좌측 눈밑 영역정보x"
+        elsif filter.include?("leu_y")
+          filter = "좌측 눈밑 영역정보y"
+        elsif filter.include?("leu_w")
+          filter = "좌측 눈밑 영역정보w"
+        elsif filter.include?("leu_h")
+          filter = "좌측 눈밑 영역정보h"
+        elsif filter.include?("rs_x")
+          filter = "우측 볼 영역정보x"
+        elsif filter.include?("rs_y")
+          filter = "우측 볼 영역정보y"
+        elsif filter.include?("rs_w")
+          filter = "우측 볼 영역정보w"
+        elsif filter.include?("rs_h")
+          filter = "우측 볼 영역정보h"
+        elsif filter.include?("ls_x")
+          filter = "좌측 볼 영역정보x"
+        elsif filter.include?("ls_y")
+          filter = "좌측 볼 영역정보y"
+        elsif filter.include?("ls_w")
+          filter = "좌측 볼 영역정보w"
+        elsif filter.include?("ls_h")
+          filter = "좌측 볼 영역정보h"
+        elsif filter.include?("rt_re_l")
+          filter = "우측눈 사각영역 정보 Left"
+        elsif filter.include?("rt_re_t")
+          filter = "우측눈 사각영역 정보 top"
+        elsif filter.include?("rt_re_r")
+          filter = "우측눈 사각영역 정보 Right"
+        elsif filter.include?("rt_re_b")
+          filter = "우측눈 사각영역 정보 Bottom"
+        elsif filter.include?("rt_le_l")
+          filter = "좌측눈 사각영역 정보 Left"
+        elsif filter.include?("rt_le_t")
+          filter = "좌측눈 사각영역 정보 top"
+        elsif filter.include?("rt_le_r")
+          filter = "좌측눈 사각영역 정보 Right"
+        elsif filter.include?("rt_le_b")
+          filter = "좌측눈 사각영역 정보 Right"
+        elsif filter.include?("rt_lip_l")
+          filter = "입 사각영역 정보 Left"
+        elsif filter.include?("rt_lip_t")
+          filter = "입 사각영역 정보 top"
+        elsif filter.include?("rt_lip_r")
+          filter = "입 사각영역 정보 Right"
+        elsif filter.include?("rt_lip_b")
+          filter = "입 사각영역 정보 Bottom"
+        elsif filter.include?("rt_res_x")
+          filter = "우측 얼굴 끝 라인 좌표x"
+        elsif filter.include?("rt_res_y")
+          filter = "우측 얼굴 끝 라인 좌표y"
+        elsif filter.include?("rt_res_w")
+          filter = "우측 얼굴 끝 라인 좌표w"
+        elsif filter.include?("rt_res_h")
+          filter = "우측 얼굴 끝 라인 좌표h"
+        else
+          filter =  filter
+        end
         @excel_name << filter
       end
     end
