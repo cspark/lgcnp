73a7e9599ad5bcb01633facf936a8051c6b231c2
mint <mint@mintui-MacBook-Pro-2.local>
2017-04-07 20:26:03 +0900

    edit

--- app/models/fcafterinterviewrx.rb
+++ app/models/fcafterinterviewrx.rb
@@ -1,2 +1,4 @@
 class Fcafterinterviewrx < ApplicationRecord
+  self.table_name = "fcafterinterviewrx" if Rails.env.production? || Rails.env.staging?
+  self.primary_key = :after_interview_id if Rails.env.production?  || Rails.env.staging?
 end
