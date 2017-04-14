1e2f24dd8bf73cb19d6f5a0f074f0920a9b6c3d1
mint <mint@mintui-MacBook-Pro-2.local>
2017-04-14 19:29:34 +0900

    edit

--- app/models/fcschedule.rb
+++ app/models/fcschedule.rb
@@ -1,6 +1,6 @@
 class Fcschedule < ApplicationRecord
   self.table_name = "fcschedule" if Rails.env.production? || Rails.env.staging?
-  self.primary_key = :ch_cd,:shop_cd,:reserve_yyyy,:reserve_mmdd, :reserve_hhmm if Rails.env.production? || Rails.env.staging?
+  self.primary_key = :ch_cd,:reserve_mmdd,:reserve_hhmm if Rails.env.production? || Rails.env.staging?
 
   def to_api_hash
     {
