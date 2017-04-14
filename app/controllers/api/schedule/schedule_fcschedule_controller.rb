7e84252cd24ca23b31022e9ff2ef28d61c839304
mint <mint@mintui-MacBook-Pro-2.local>
2017-04-14 15:30:20 +0900

    Edit

--- app/controllers/api/schedule/schedule_fcschedule_controller.rb
+++ app/controllers/api/schedule/schedule_fcschedule_controller.rb
@@ -2,7 +2,7 @@ class Api::Schedule::ScheduleFcscheduleController < Api::ApplicationController
   def index
     # Calendar 에서 월 선택 시 해당 월에 예약이 존재하는지 확인 RESERVE_MMDD 오름차순으로 정렬
     # 년월일, 채널코드, 매장코드 조건 (* Next 조회 필요)
-    list = Fcschedule.where(ch_cd: params[:ch_cd], shop_cd: params[:shop_cd], reserve_yyyy: params[:reserve_yyyy], reserve_mmdd: params[:reserve_mmdd])
+    list = Fcschedule.where(ch_cd: params[:ch_cd], shop_cd: params[:shop_cd], reserve_yyyy: params[:reserve_yyyy], reserve_mmdd: params[:reserve_mmdd], reserve_hhmm: params[:reserve_hhmm])
     if list.count > 0
       render json: api_hash_for_list(list), status: :ok
     else
