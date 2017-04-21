f9527ed1c3488950794453af5216f4166019b7f5
mint <mint@mintui-MacBook-Pro-2.local>
2017-04-21 14:14:55 +0900

    edit

--- app/controllers/api/beau/beau_user_controller.rb
+++ app/controllers/api/beau/beau_user_controller.rb
@@ -10,7 +10,9 @@ class Api::Beau::BeauUserController < Api::ApplicationController
 
   def lcare_user_list
     # L-Care Serial 조건으로 Janus3 DB에 해당 L-Care 회원이 존재하는지 확인
-    user = Custinfo.where(n_cust_id: params[:n_cust_id]).first
+    # 106288804  김수민
+    # 105598288  태영님
+    user = Custinfo.where(n_cust_id: params[:n_cust_id]).where.not(ch_cd: "CNPR").where.not(ch_cd: "RLAB").first
     if !user.nil?
       render json: user.to_api_hash_for_yanus, status: :ok
     else
