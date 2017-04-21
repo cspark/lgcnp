b935916292fdb7aa0d13c3e52aafcc912cdb778c
mint <mint@mintui-MacBook-Pro-2.local>
2017-04-21 17:15:56 +0900

    Edit

---
+++ db/migrate/20170421054456_change_column_to_fcafterinterviewrx.rb
@@ -0,0 +1,5 @@
+class ChangeColumnToFcafterinterviewrx < ActiveRecord::Migration[5.0]
+  def change
+    change_column :fcafterinterviewrxes, :a6, :string
+  end
+end
---
+++ db/migrate/20170421054630_add_column_to_fcafterinterviewrx.rb
@@ -0,0 +1,5 @@
+class AddColumnToFcafterinterviewrx < ActiveRecord::Migration[5.0]
+  def change
+    add_column :fcafterinterviewrxes, :a6_1, :text
+  end
+end
