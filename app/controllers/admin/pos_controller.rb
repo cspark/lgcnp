d7625c5f91ba9d664281afb88a44f30666a06ed5
mint <mint@mintui-MacBook-Pro-2.local>
2017-05-16 00:54:24 +0900

    Edit

--- app/controllers/admin/pos_controller.rb
+++ app/controllers/admin/pos_controller.rb
@@ -28,13 +28,13 @@ class Admin::PosController < Admin::AdminApplicationController
         user_custserials.push(user.custserial)
       end
       if @ch_cd != "ALL"
-        @fcpos = Fcpos.where("ch_cd LIKE ?", "%#{@ch_cd}%").where(custserial: user_custserials).order("uptdate desc")
+        @fcpos = Fcpos.where(ch_cd: @ch_cd).where(custserial: user_custserials).order("uptdate desc")
       else
         @fcpos = Fcpos.where(custserial: user_custserials).order("uptdate desc")
       end
     else
       if @ch_cd != "ALL"
-        @fcpos = Fcpos.where("ch_cd LIKE ?", "%#{@ch_cd}%").order("uptdate desc")
+        @fcpos = Fcpos.where(ch_cd: @ch_cd).order("uptdate desc")
       else
         @fcpos = Fcpos.order("uptdate desc")
       end
