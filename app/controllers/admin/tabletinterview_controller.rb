2d7ac91696c13eaa817095799d33a2e9273b8f80
mint <mint@mintui-MacBook-Pro-2.local>
2017-05-15 17:49:03 +0900

    Edit

--- app/controllers/admin/tabletinterview_controller.rb
+++ app/controllers/admin/tabletinterview_controller.rb
@@ -436,7 +436,7 @@ class Admin::TabletinterviewController < Admin::AdminApplicationController
 
     @count = @beau_interviews.count
     @beau_interviews_excel = @beau_interviews
-    @beau_interviews = Kaminari.paginate_array(@beau_interviews).page(params[:page]).per(3)
+    @beau_interviews = Kaminari.paginate_array(@beau_interviews).page(params[:page]).per(5)
 
     respond_to do |format|
       format.html
