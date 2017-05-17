f8ae8c555fabb19d7eb6e901a044d57ffa9fe414
mint <mint@mintui-MacBook-Pro-2.local>
2017-05-17 17:10:21 +0900

    Edit

--- app/models/allowaccess.rb
+++ app/models/allowaccess.rb
@@ -1,12 +1,4 @@
 class Allowaccess < ApplicationRecord
   self.table_name = "allowaccess" if Rails.env.production? || Rails.env.staging?
   self.primary_key = :low_ip,:high_ip if Rails.env.production? || Rails.env.staging?
-
-  def update_ip(low_ip: nil, high_ip: nil)
-    Rails.logger.info low_ip
-    Rails.logger.info high_ip
-    self.low_ip = low_ip
-    self.high_ip = high_ip
-    self.save
-  end
 end
