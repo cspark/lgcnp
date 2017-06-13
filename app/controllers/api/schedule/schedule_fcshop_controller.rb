8834e7f2a118b2b8e7ce6f26ca127c49ed558592
mint <mint@mintui-MacBook-Pro-2.local>
2017-06-13 21:19:15 +0900

    Edit

--- app/controllers/api/schedule/schedule_fcshop_controller.rb
+++ app/controllers/api/schedule/schedule_fcshop_controller.rb
@@ -1,7 +1,11 @@
 class Api::Schedule::ScheduleFcshopController < Api::ApplicationController
   def index
     # Config에서 최초 매장코드 설정을 위하여 매장 테이블 전체 조회 (* Next 조회 필요)
-    list = Fcshop.all.order("shop_cd asc")
+    if params.has_key?(:ch_cd)
+      list = Fcshop.where(ch_cd: params[:ch_cd]).order("shop_name asc")
+    else
+      list = Fcshop.all.order("shop_name asc")
+    end
     render json: api_hash_for_list(list), status: :ok
   end
 end
