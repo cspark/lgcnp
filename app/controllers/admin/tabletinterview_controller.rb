1a517ee3091c3e7aa3f19428dc112dc131793057
mint <mint@mintui-MacBook-Pro-2.local>
2017-06-22 19:50:15 +0900

    Edit

--- app/controllers/admin/tabletinterview_controller.rb
+++ app/controllers/admin/tabletinterview_controller.rb
@@ -87,12 +87,18 @@ class Admin::TabletinterviewController < Admin::AdminApplicationController
     scoped = scoped.where("faceno LIKE ?", "%#{@select_area}%") if !@select_area.blank? && @select_area != "all"
     scoped = scoped.where(shop_cd: @shop_cd) if !@shop_cd.blank?
     fcdata_list = scoped
-    serial_array = fcdata_list.where("CAST(custserial AS INT) < ? ", 1001).pluck(:custserial).uniq
-    serial_array2 = fcdata_list.where("CAST(custserial AS INT) > ? AND CAST(custserial AS INT) < ? ", 1000, 2001).pluck(:custserial).uniq
-    serial_array3 = fcdata_list.where("CAST(custserial AS INT) > ? AND CAST(custserial AS INT) < ? ", 2000, 3001).pluck(:custserial).uniq
-    measureno_array = fcdata_list.pluck(:measureno).map(&:to_i).uniq
 
-    scoped = Fctabletinterview.all
+    temp_serial_array = fcdata_list.where("CAST(custserial AS INT) < ? ", 1001).pluck(:custserial).uniq
+    temp_serial_array2 = fcdata_list.where("CAST(custserial AS INT) > ? AND CAST(custserial AS INT) < ? ", 1000, 2001).pluck(:custserial).uniq
+    temp_serial_array3 = fcdata_list.where("CAST(custserial AS INT) > ? AND CAST(custserial AS INT) < ? ", 2000, 3001).pluck(:custserial).uniq
+    temp_measureno_array = fcdata_list.pluck(:measureno).map(&:to_i).uniq
+
+    tablet_interviews = Fctabletinterview.where(custserial: temp_serial_array).where(fcdata_id: temp_measureno_array).where.not(before_serum: nil)
+    tablet_interviews2 = Fctabletinterview.where(custserial: temp_serial_array2).where(fcdata_id: temp_measureno_array).where.not(before_serum: nil)
+    tablet_interviews3 = Fctabletinterview.where(custserial: temp_serial_array3).where(fcdata_id: temp_measureno_array).where.not(before_serum: nil)
+    tablet_interviews = tablet_interviews.or(tablet_interviews2).or(tablet_interviews3)
+
+    scoped = tablet_interviews
     temp_end_date = @end_date.to_date+1.day
     if Rails.env.production? || Rails.env.staging?
       scoped = scoped.where("to_date(uptdate) >= ? AND to_date(uptdate) < ?", @start_date.to_date, temp_end_date)
@@ -522,11 +528,6 @@ class Admin::TabletinterviewController < Admin::AdminApplicationController
     serial_array2 = fcdata_list.where("CAST(custserial AS INT) > ? AND CAST(custserial AS INT) < ? ", 1000, 2001).pluck(:custserial).uniq
     serial_array3 = fcdata_list.where("CAST(custserial AS INT) > ? AND CAST(custserial AS INT) < ? ", 2000, 3001).pluck(:custserial).uniq
     measureno_array = fcdata_list.pluck(:measureno).map(&:to_i).uniq
-
-    Rails.logger.info "scoped.count!!!"
-    Rails.logger.info scoped.count
-    Rails.logger.info serial_array
-    Rails.logger.info measureno_array
 
     scoped = Fctabletinterviewrx.where(custserial: serial_array).where(fcdata_id: measureno_array)
     scoped = scoped.or(Fctabletinterviewrx.where(custserial: serial_array2).where(fcdata_id: measureno_array)).or(Fctabletinterviewrx.where(custserial: serial_array3).where(fcdata_id: measureno_array))
