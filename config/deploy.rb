8496a33730f74e154926c098aadcb266e4554ac1
mint <mint@mintui-MacBook-Pro-2.local>
2017-06-23 20:08:31 +0900

    Edit

--- config/deploy.rb
+++ config/deploy.rb
@@ -15,7 +15,10 @@ set :rbenv_roles, :all # default value
 # set :linked_dirs, %w{log public/CNP}
 
 set :linked_dirs, fetch(:linked_dirs, []).push('public/TEST', 'log', 'public/TEST')
+set :linked_dirs, fetch(:linked_dirs, []).push('public/CNPR', 'log', 'public/BEAU')
+set :linked_dirs, fetch(:linked_dirs, []).push('public/CNPR', 'log', 'public/CNP')
 set :linked_dirs, fetch(:linked_dirs, []).push('public/CNPR', 'log', 'public/CNPR')
+set :linked_dirs, fetch(:linked_dirs, []).push('public/CNPR', 'log', 'public/RLAB')
 
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 # set :deploy_to, '/var/www/my_app'
