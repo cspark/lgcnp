78f7c89a38f09ed88b21c692c62d9913837fbd66
mint <mint@soominui-MacBook-Pro.local>
2017-07-31 20:29:40 +0900

    edit

--- app/models/privacyaccesshistory.rb
+++ app/models/privacyaccesshistory.rb
@@ -1,4 +1,3 @@
 class Privacyaccesshistory < ApplicationRecord
   self.table_name = "Privacyaccesshistory" if Rails.env.production? || Rails.env.staging?
-  self.primary_key = :id if Rails.env.production? || Rails.env.staging?
 end
