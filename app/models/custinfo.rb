4d0800df1130f935aa092b16c6b3c7fe8b7fc532
mint <mint@soominui-MacBook-Pro.local>
2017-09-13 16:13:21 +0900

    edit

--- app/models/custinfo.rb
+++ app/models/custinfo.rb
@@ -34,7 +34,7 @@ class Custinfo < ApplicationRecord
       custserial: custserial,
       ch_cd: ch_cd,
       n_cust_id: n_cust_id,
-      measureno: measureno.to_s,
+      measureno: measureno,
       custname: custname,
       sex: sex,
       age: age,
