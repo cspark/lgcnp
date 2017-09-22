a51202110c1385d472f015e1e14e9dcaee8acd88
mint <mint@soominui-MacBook-Pro.local>
2017-09-22 17:26:09 +0900

    Devise ssh512 변경 작업

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,9 +1,11 @@
+require 'bcrypt'
+
 class AdminUser < ApplicationRecord
   self.table_name = "adminuser" if Rails.env.production? || Rails.env.staging?
   self.primary_key = :email if Rails.env.production? || Rails.env.staging?
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable,
-         :recoverable, :rememberable, :trackable
+         :recoverable, :rememberable, :trackable, :encryptable
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170907005656) do
+ActiveRecord::Schema.define(version: 20170921101638) do
 
   create_table "admin_users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20170907005656) do
     t.string   "ch_cd"
     t.string   "shop_cd"
     t.string   "role"
+    t.string   "password_salt"
     t.index ["email"], name: "index_admin_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
   end
