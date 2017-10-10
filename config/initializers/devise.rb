a774208439e1ea3a605a037874d3d4780aab0a9f
mint <mint@soominui-MacBook-Pro.local>
2017-10-10 11:13:41 +0900

    Edit

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -3,10 +3,22 @@ require "digest/sha2"
 module Devise
   module Encryptable
     module Encryptors
-      class CustomAuthentication < Base
+      # = Sha512
+      # Uses the Sha512 hash algorithm to encrypt passwords.
+      class Sha512 < Base
+        # Generates a default password digest based on salt, pepper and the
+        # incoming password.
         def self.digest(password, stretches, salt, pepper)
-          string_to_hash = "#{pepper}#{salt}#{password.reverse}"
-          Digest::SHA2.hexdigest(string_to_hash)
+          digest = pepper
+          stretches.times { digest = self.secure_digest(salt, digest, password, pepper) }
+          digest
+        end
+
+      private
+        # Generate a Sha512 digest joining args. Generated token is something like
+        #   --arg1--arg2--arg3--argN--
+        def self.secure_digest(*tokens)
+          ::Digest::SHA512.hexdigest('--' << tokens.flatten.join('--') << '--')
         end
       end
     end
@@ -228,7 +240,7 @@ Devise.setup do |config|
   # stretches to 10, and copy REST_AUTH_SITE_KEY to pepper).
   #
   # Require the `devise-encryptable` gem when using anything other than bcrypt
-  config.encryptor = :custom_authentication
+  config.encryptor = :Sha512
 
   # ==> Scopes configuration
   # Turn scoped views on. Before rendering "sessions/new", it will first check for
