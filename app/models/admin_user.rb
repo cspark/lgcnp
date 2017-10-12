7aeebc5982319b3a70458e9a70fe3ec291006394
mint <mint@soominui-MacBook-Pro.local>
2017-10-12 15:32:55 +0900

    edit

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -6,6 +6,6 @@ class AdminUser < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable,
-         :recoverable, :rememberable, :trackable
+         :recoverable, :rememberable, :trackable, :encryptable
 
 end
