1db78c18deb6cc713c3a0c15076fbe47fe9d5b68
Ray Jung <jung.changhwan@mintech.kr>
2017-11-09 13:19:46 +0900

    Edited

--- app/controllers/api/beau/beau_fcavgdata_controller.rb
+++ app/controllers/api/beau/beau_fcavgdata_controller.rb
@@ -2,11 +2,16 @@ class Api::Beau::BeauFcavgdataController < Api::ApplicationController
   def index
     if params.has_key?(:his_date)
       his_date = params[:his_date].to_date
-      list = FcavgdataHistory.where("to_date(his_start_date) =< ? AND to_date(his_end_date) > ?", his_date, his_date).order("n_index asc")
+      list = FcavgdataHistory.where("? BETWEEN to_date(his_start_date) AND to_date(his_end_date)", his_date).order("n_index asc")
       if list.count > 0
         render json: api_hash_for_list(list), status: :ok
       else
-        render :text => "Fcavgdata is not exist!!!", status: 204
+        list = Fcavgdata.all.order("n_index asc")
+        if list.count > 0
+          render json: api_hash_for_list(list), status: :ok
+        else
+          render :text => "Fcavgdata is not exist!!!", status: 204
+        end
       end
     else
       list = Fcavgdata.all.order("n_index asc")
