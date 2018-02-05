f3b244e92f19173b89c3e38f6b959fecc2e533dd
Ray Jung <jung.changhwan@mintech.kr>
2018-02-05 15:42:53 +0900

    Edited

--- app/controllers/admin/data_controller.rb
+++ app/controllers/admin/data_controller.rb
@@ -1132,27 +1132,6 @@ class Admin::DataController < Admin::AdminApplicationController
       scoped = scoped.where(skintype: @select_skin_type_device_final)
     end
 
-    if Rails.env.production? || Rails.env.staging?
-      scoped = scoped.joins(:custinfo).where("custinfo.custname LIKE ?", "%#{@name}%") if !@name.nil?
-      scoped = scoped.joins(:custinfo).where("custinfo.sex LIKE ?", "%#{@select_sex}%") if @select_sex != "all"
-      if !@start_age.blank? && !@end_age.blank?
-        start_birthyy = Time.current.year.to_i - @start_age.to_i
-        end_birthyy = Time.current.year.to_i - @end_age.to_i
-        scoped = scoped.joins(:custinfo).where("to_number(custinfo.birthyy) >= ? AND to_number(custinfo.birthyy) < ?", end_birthyy, start_birthyy)
-      end
-
-      scoped = scoped.joins(:custinfo).where("to_number(custinfo.birthyy) >= ? AND to_number(custinfo.birthyy) < ?", @start_birthyy, @end_birthyy) if !@start_birthyy.blank? && !@end_birthyy.blank?
-      scoped = scoped.joins(:custinfo).where("to_number(custinfo.birthmm) >= ? AND to_number(custinfo.birthmm) < ?", @start_birthmm, @end_birthmm) if !@start_birthmm.blank? && !@end_birthmm.blank?
-
-      if params.has_key?(:is_agree_thirdparty_info)
-        if params[:is_agree_thirdparty_info].empty?
-          scoped = scoped.joins(:custinfo).where("custinfo.is_agree_thirdparty_info LIKE ?", nil)
-        else
-          scoped = scoped.joins(:custinfo).where("custinfo.is_agree_thirdparty_info LIKE ?", "%#{params[:is_agree_thirdparty_info]}%") if params[:is_agree_thirdparty_info] != "T,F"
-        end
-      end
-    end
-
     scoped = scoped.order("fcdata.measuredate desc")
 
     if @select_filter == []
@@ -1356,6 +1335,54 @@ class Admin::DataController < Admin::AdminApplicationController
           filter = filter
         end
         @excel_name << filter
+      end
+    end
+
+    scoped.each do |fcdata|
+      custinfo = Custinfo.where(custserial: fcdata.custserial).first
+      is_contain = true
+
+      if !custinfo.nil?
+        if !@name.blank? && !custinfo.custname.nil?
+          if !custinfo.custname.include? @name
+            is_contain = false
+          end
+        end
+
+        if @select_sex != "all"
+          if custinfo.sex != @select_sex
+            is_contain = false
+          end
+        end
+
+        if !@start_age.blank? && !@end_age.blank?
+          temp_age = Time.current.year.to_i - custinfo.birthyy.to_i
+          if temp_age < @start_age.to_i || temp_age > @end_age.to_i
+            is_contain = false
+          end
+        end
+
+        if !@start_birthyy.blank? && !@end_birthyy.blank?
+          if custinfo.birthyy.to_i < @start_birthyy.to_i || custinfo.birthyy.to_i > @end_birthyy.to_i
+            is_contain = false
+          end
+        end
+
+        if !@start_birthmm.blank? && !@end_birthmm.blank?
+          if custinfo.birthmm.to_i < @start_birthmm.to_i || custinfo.birthmm.to_i > @end_birthmm.to_i
+            is_contain = false
+          end
+        end
+
+        if custinfo.nil?
+          is_contain = false
+        end
+      else
+        is_contain = false
+      end
+
+      if is_contain == true
+        @fcdatas << fcdata
       end
     end
 
