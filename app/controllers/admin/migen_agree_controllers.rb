8072df51438a886f87d47ea55173b947acc7fa42
Sooyong Wang <wangsy@wangsy.com>
2018-05-13 19:10:07 +0900

    fix error

--- app/controllers/admin/migen_agree_controllers.rb
+++ app/controllers/admin/migen_agree_controllers.rb
@@ -1,7 +1,6 @@
 class Admin::MigenAgreeController < Admin::AdminApplicationController
   def index
     @count = FcAgreeMigen.count
-    @migen_agree = FcAgreeMigen.all
-    @migen_agree = Kaminari.paginate(@migen_agree).page(params[:page]).per(10)
+    @migen_agree = FcAgreeMigen.page(params[:page]).per(10)
   end
 end
