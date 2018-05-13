0b279031fa388ab9d65ded0c93cd7c2e4b8aa13e
Sooyong Wang <wangsy@wangsy.com>
2018-05-13 20:40:37 +0900

    FC AGREE MIGEN #2

--- app/controllers/admin/migen_agree_controllers.rb
+++ app/controllers/admin/migen_agree_controllers.rb
@@ -1,6 +1,27 @@
 class Admin::MigenAgreeController < Admin::AdminApplicationController
   def index
-    @count = FcAgreeMigen.count
-    @migen_agree = FcAgreeMigen.page(params[:page]).per(10)
+    @custserial = params[:custserial]
+    if session[:admin_user] == "user" || (!session[:admin_user]['role'].nil? && session[:admin_user]['role'] == "admin")
+      ch_cd = ""
+      shop_cd = ""
+    else
+      ch_cd = session[:admin_user]['ch_cd']
+      shop_cd = session[:admin_user]['shop_cd']
+    end
+    ch_cd = params[:select_channel] if !params[:select_channel].nil?
+    shop_cd = params[:select_shop]
+    @shop_cd = shop_cd if !shop_cd.blank?
+    @ch_cd = ch_cd if !ch_cd.blank?
+
+    scoped = FcAgreeMigen.all
+    scoped = scoped.where("shop_cd LIKE '%#{@shop_cd}%'") if !@shop_cd.blank?
+    scoped = scoped.where(ch_cd: @ch_cd) if !@ch_cd.blank? && @ch_cd != "ALL"
+    scoped = scoped.where(custserial: @custserial) if !@custserial.blank?
+    @search = ""
+    @search = params[:search] if params.has_key?(:search) && params[:search].length != 0
+    scoped = scoped.where("custname LIKE ?", "%#{@search}%") if !@search.blank?
+
+    @count = scoped.count
+    @migen_agree = scoped.page(params[:page]).per(10)
   end
 end
